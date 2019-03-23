6d30fa583effb6eb08895be11803f29d6f97b3fa
nelotgrom <nelotgrom@gmail.com>
2019-03-23 15:54:30 +0300

    file: Complited task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  var count = 0; 
+  for (var i = 0; i <= preferences.length; i++) {
+    if ((preferences[preferences[preferences[i] - 1] - 1] - 1 == i) && (i + 1 !== preferences[i])) {
+      count += 1; 
+    } 
+  };
+  return count / 3;
+}; 
